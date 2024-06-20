64f1cdc7ff178e376060d4059a01313ab9711382
SLDrago <dilshanoshada7@gmail.com>
2024-06-20 14:37:29 +0530

    login role selector update

--- src/components/Authentication/UserShopRadio.tsx
+++ src/components/Authentication/UserShopRadio.tsx
@@ -4,18 +4,22 @@ import { UserIcon, BuildingStorefrontIcon } from "@heroicons/react/24/solid";
 
 interface UserShopRadioProps {
   name: string;
+  onChange: (value: string) => void;
 }
 
-const UserShopRadio: React.FC<UserShopRadioProps> = ({ name }) => {
+const UserShopRadio: React.FC<UserShopRadioProps> = ({ name, onChange }) => {
   const [selected, setSelected] = useState<"user" | "shop">("user");
-
+  const handleSelection = (value: "user" | "shop") => {
+    setSelected(value);
+    onChange(value);
+  };
   return (
     <div className="flex justify-center items-center">
       <div className="flex space-x-4">
         <Button
           color={selected === "user" ? "black" : "gray"}
           variant={selected === "user" ? "filled" : "outlined"}
-          onClick={() => setSelected("user")}
+          onClick={() => handleSelection("user")}
           className="flex items-center space-x-2"
         >
           <UserIcon className="h-5 w-5" />
@@ -24,7 +28,7 @@ const UserShopRadio: React.FC<UserShopRadioProps> = ({ name }) => {
         <Button
           color={selected === "shop" ? "black" : "gray"}
           variant={selected === "shop" ? "filled" : "outlined"}
-          onClick={() => setSelected("shop")}
+          onClick={() => handleSelection("shop")}
           className="flex items-center space-x-2"
         >
           <BuildingStorefrontIcon className="h-5 w-5" />
